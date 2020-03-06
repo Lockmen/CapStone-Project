3ee183ce5fbfdf4a98b88eb77a460d0129cb41a0
Jin-Woo Kim <dnehd6739@naver.com>
2020-03-06 18:53:13 +0900

    구현 90%완료, 플레이어 체력 구현, 점수판 구현, 플레인 디테일 수정 완료 남은것(조이스틱으로 총발사 및 플레이어 시점 변경 구현하면 끝)

--- New Unity Project/Assets/Scripts/Enemy.cs
+++ New Unity Project/Assets/Scripts/Enemy.cs
@@ -10,6 +10,7 @@ public class Enemy : LivingEntity
     State currentState;
 
     public ParticleSystem deathEffect;
+    public static event System.Action OnDeathStatic;
 
     NavMeshAgent pathfinder;
     Transform target;
@@ -28,33 +29,58 @@ public class Enemy : LivingEntity
 
     bool hasTarget;
 
-    protected override void Start()
+    void Awake()
     {
-        base.Start();
         pathfinder = GetComponent<NavMeshAgent>();
-        skinMaterial = GetComponent<Renderer>().material;
-        originalColour = skinMaterial.color;
 
         if (GameObject.FindGameObjectWithTag("Player") != null)
         {
-            currentState = State.Chasing;
             hasTarget = true;
 
             target = GameObject.FindGameObjectWithTag("Player").transform;
             targetEntity = target.GetComponent<LivingEntity>();
-            targetEntity.OnDeath += OnTargetDeath;
 
             myCollisionRadius = GetComponent<CapsuleCollider>().radius;
             targetCollisionRadius = target.GetComponent<CapsuleCollider>().radius;
+        }
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+
+        if (hasTarget)
+        {
+            currentState = State.Chasing;
+            targetEntity.OnDeath += OnTargetDeath;
 
             StartCoroutine(UpdatePath());
         }
+    }
+
+    public void SetCharacteristics(float moveSpeed, int hitsToKillPlayer, float enemyHealth, Color skinColour)
+    {
+        pathfinder.speed = moveSpeed;
+
+        if (hasTarget)
+        {
+            damage = Mathf.Ceil(targetEntity.startingHealth / hitsToKillPlayer);
+        }
+        startingHealth = enemyHealth;
+
+        skinMaterial = GetComponent<Renderer>().material;
+        skinMaterial.color = skinColour;
+        originalColour = skinMaterial.color;
     }
 
     public override void TakeHit(float damage, Vector3 hitPoint, Vector3 hitDirection)
     {
         if (damage >= health)
         {
+            if(OnDeathStatic != null)
+            {
+                OnDeathStatic();
+            }
             Destroy(Instantiate(deathEffect.gameObject, hitPoint, Quaternion.FromToRotation(Vector3.forward, hitDirection)) as GameObject, deathEffect.startLifetime);
         }
         base.TakeHit(damage, hitPoint, hitDirection);
--- New Unity Project/Assets/Scripts/GameUI.cs
+++ New Unity Project/Assets/Scripts/GameUI.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using UnityEngine.SceneManagement;
+
 
 public class GameUI : MonoBehaviour
 {
@@ -8,15 +10,84 @@ public class GameUI : MonoBehaviour
     public Image fadePlane;
     public GameObject gameOverUI;
 
+    public RectTransform newWaveBanner;
+    public Text newWaveTitle;
+    public Text newWaveEnemyCount;
+    public Text scoreUI;
+    public Text gameOverScoreUI;
+    public RectTransform healthBar;
+
+    Spawner spawner;
+    Player player;
+
     void Start()
     {
-        FindObjectOfType<Player>().OnDeath += OnGameOver;
+        player = FindObjectOfType<Player>();
+        player.OnDeath += OnGameOver;
+    }
+
+    void Awake()
+    {
+        spawner = FindObjectOfType<Spawner>();
+        spawner.OnNewWave += OnNewWave;
+    }
+
+    void Update()
+    {
+        scoreUI.text = ScoreKeeper.score.ToString("D6");
+        float healthPercent = 0;
+        if (player != null)
+        {
+            healthPercent = player.health / player.startingHealth;
+        }
+        healthBar.localScale = new Vector3(healthPercent, 1, 1);
+    }
+
+
+    void OnNewWave(int waveNumber)
+    {
+        string[] numbers = { "1", "2", "3", "4", "5" };
+        newWaveTitle.text = "- 레벨 " + numbers[waveNumber - 1] + " -";
+        string enemyCountString = ((spawner.waves[waveNumber - 1].infinite) ? "무한생성" : spawner.waves[waveNumber - 1].enemyCount + "");
+        newWaveEnemyCount.text = "적 : " + enemyCountString + "마리";
+
+        StopCoroutine("AnimateNewWaveBanner");
+        StartCoroutine("AnimateNewWaveBanner");
     }
 
     void OnGameOver()
     {
         StartCoroutine(Fade(Color.clear, Color.black, 1));
         gameOverUI.SetActive(true);
+    }
+
+    IEnumerator AnimateNewWaveBanner()
+    {
+
+        float delayTime = 1.5f;
+        float speed = 3f;
+        float animatePercent = 0;
+        int dir = 1;
+
+        float endDelayTime = Time.time + 1 / speed + delayTime;
+
+        while (animatePercent >= 0)
+        {
+            animatePercent += Time.deltaTime * speed * dir;
+
+            if (animatePercent >= 1)
+            {
+                animatePercent = 1;
+                if (Time.time > endDelayTime)
+                {
+                    dir = -1;
+                }
+            }
+
+            newWaveBanner.anchoredPosition = Vector2.up * Mathf.Lerp(-300, 45, animatePercent);
+            yield return null;
+        }
+
     }
 
     IEnumerator Fade(Color from, Color to, float time)
@@ -35,6 +106,6 @@ public class GameUI : MonoBehaviour
     // UI Input
     public void StartNewGame()
     {
-        Application.LoadLevel("Game");
+        SceneManager.LoadScene("Game");
     }
 }
